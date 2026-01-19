d45ed6c81e7a6701f7600eac6d22b05f46ea762e
Timothy jan <jan.timothy@gmail.com>
2026-01-19 11:27:34 -0800

    Footer Complete

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./sections/header/header";
+import { Footer } from './sections/footer/footer';
 import { Intro } from "./sections/intro/intro";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Intro],
+  imports: [
+    Header,
+    Footer,
+    Intro
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
